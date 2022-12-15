1b9c9ddc0a7c4e3f29381681be3ed22149c2c02f
Vitor Fonseca <vitor.fonseca@kuehne-nagel.com>
2022-12-15 16:43:06 +0000

    Constraint around low resolution

--- pigeon/src/App.vue
+++ pigeon/src/App.vue
@@ -1,16 +1,38 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
 import { RouterView } from "vue-router";
 import Topbar from "./components/Topbar/Topbar.vue";
+
+const resolutionIsTooLow = ref(false);
+
+const checkIfResolutionIsLow = () =>
+  (resolutionIsTooLow.value = window.innerWidth < 540);
+
+onMounted(checkIfResolutionIsLow);
+
+window.addEventListener("resize", checkIfResolutionIsLow);
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <Topbar />
-    </div>
-  </header>
+  <div v-if="resolutionIsTooLow" class="low-resolution-message">
+    <h1>Sorry, this app isn't supported in low resolutions</h1>
+  </div>
+  <div v-else>
+    <header>
+      <div class="wrapper">
+        <Topbar />
+      </div>
+    </header>
 
-  <RouterView />
+    <RouterView />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.low-resolution-message {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  height: 100vh;
+}
+</style>
