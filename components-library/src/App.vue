6407b47f5d1a87b8c886b983e8200570b27eff15
Vitor Fonseca <vitor.fonseca@kuehne-nagel.com>
2022-12-19 10:33:06 +0000

    Components project boilerplate and button component

---
+++ components-library/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Button from "@/components/Button/Button.vue";
+</script>
+
+<template>
+  <Button>Click me</Button>
+</template>
+
+<style scoped></style>
---
+++ components-library/src/components/Button/Button.vue
@@ -0,0 +1,9 @@
+<template>
+  <el-button type="primary"><slot /></el-button>
+</template>
+
+<script lang="ts" setup>
+import { ElButton } from "element-plus";
+</script>
+
+<style lang="scss" scoped></style>
