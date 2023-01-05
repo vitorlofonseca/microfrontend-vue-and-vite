9b9779f5a126d17885dc0a91fadd14cfed3f860a
Vitor Fonseca <vitor.fonseca@kuehne-nagel.com>
2023-01-05 11:18:21 +0000

    Binding all props component receives

--- components-library/lib/Button/Button.vue
+++ components-library/lib/Button/Button.vue
@@ -1,27 +1,9 @@
 <script setup lang="ts">
 import { ElButton } from "element-plus";
-import { EpPropMergeType } from "element-plus/es/utils";
-
-type Props = {
-  type: EpPropMergeType<
-    StringConstructor,
-    | ""
-    | "default"
-    | "primary"
-    | "success"
-    | "warning"
-    | "info"
-    | "danger"
-    | "text",
-    unknown
-  >;
-};
-
-const props = defineProps<Props>();
 </script>
 
 <template>
-  <el-button :type="props.type"><slot /></el-button>
+  <el-button v-bind="$props"><slot /></el-button>
 </template>
 
 <style scoped></style>
