6c74fe59b3dde9797dd1389d2138eccd4ca27a7c
Vitor Fonseca <vitor.fonseca@kuehne-nagel.com>
2023-01-05 11:18:42 +0000

    AgGrid custom component

--- components-library/lib/main.ts
+++ components-library/lib/main.ts
@@ -1,4 +1,5 @@
 import Button from "./Button/Button.vue";
+import Table from "./Table/Table.vue";
 import "element-plus/dist/index.css";
 
-export { Button };
+export { Button, Table };
--- components-library/src/App.vue
+++ components-library/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Button from "../lib/Button/Button.vue";
+import Table from "../lib/Table/Table.vue";
 </script>
 
 <template>
   <Button type="primary">Click here</Button>
+  <Table></Table>
 </template>
