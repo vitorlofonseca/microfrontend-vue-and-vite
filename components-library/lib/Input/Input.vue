1c192dcaedf50266d52bf712ef0848e0f59b1568
Vitor Fonseca <vitor.fonseca@kuehne-nagel.com>
2023-01-19 11:52:43 +0000

    Input component

---
+++ components-library/lib/Input/Input.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ElInput } from "element-plus";
+</script>
+
+<template>
+  <el-input v-bind="$props" />
+</template>
+
+<style scoped></style>
--- components-library/lib/main.ts
+++ components-library/lib/main.ts
@@ -1,5 +1,6 @@
 import Button from "./Button/Button.vue";
 import Table from "./Table/Table.vue";
+import Input from "./Table/Input.vue";
 import "element-plus/dist/index.css";
 
-export { Button, Table };
+export { Button, Table, Input };
