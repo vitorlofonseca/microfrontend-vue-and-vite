7b5ebbd89de8d2b3f89fec0631f95e98ba26d10a
Vitor Fonseca <vitor.fonseca@kuehne-nagel.com>
2023-01-19 13:29:08 +0000

    Bugfixing on components

--- components-library/lib/main.ts
+++ components-library/lib/main.ts
@@ -1,6 +1,6 @@
 import Button from "./Button/Button.vue";
 import Table from "./Table/Table.vue";
-import Input from "./Table/Input.vue";
+import Input from "./Input/Input.vue";
 import "element-plus/dist/index.css";
 
 export { Button, Table, Input };
